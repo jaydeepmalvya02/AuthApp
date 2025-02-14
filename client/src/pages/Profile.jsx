1ae739d2669d964f5496ca8f655789074de9f067
jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-02-14 14:14:14 +0530

    complete user update functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,26 +1,71 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { updateUserStart,updateUserSuccess,updateUserFailure } from '../../redux/user/userSlice';
 
 export default function Profile() {
-  const {currentUser}=useSelector(state=> state.user)
+  const {currentUser,loading,error}=useSelector(state=> state.user)
+  const [formData,setFormData]=useState({})
+  const dispatch=useDispatch()
+  // const [loading,setLoading]=useState(false)
+  // const [error,setError]=useState(false)
+  const [updateSuccess,setUpdateSuccess]=useState(false);
+  const handleChange=(e)=>{
+    setFormData({...formData,[e.target.id]:e.target.value});
+  }
+
+  console.log(formData) 
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+
+    try {
+
+      dispatch(updateUserStart())
+      const res=await fetch(`/api/user/update/${currentUser._id}`,{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify(formData),
+      })
+      const data = await res.json();
+      console.log(data)
+      if (data.success===false){
+        dispatch(updateUserFailure(data))
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error));
+      console.log('Error updating user', error)  // for debugging purposes only, remove in production!  // error will contain a detailed message about the error that occurred.  // you might want to show this error to the user in a more user-friendly way.  // for example, you could show an error message to the user, and then re-enable the form submission button.  // this is just a simple example, and you might want to do something more complex depending
+      
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form  className="flex flex-col gap-4">
+      <form  
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-4">
         <img src={currentUser.avatar} alt="Avatar" className="h-24 w-24 self-center rounded-full cursor-pointer object-cover mt-2" />
       <input 
+      onChange={handleChange}
       defaultValue={currentUser.username}
       type="text" 
       className="bg-slate-100 rounded-lg p-3" 
       id='username'
       placeholder='username'/>
       <input 
+      onChange={handleChange}
       defaultValue={currentUser.email}
       type="email" 
       className="bg-slate-100 rounded-lg p-3" 
       id='email'
       placeholder='email'/>
-      <input type="password" 
+      <input
+      onChange={handleChange}
+      type="password" 
       className="bg-slate-100 rounded-lg p-3" 
       id='password'
       placeholder='password'/>
@@ -31,6 +76,9 @@ export default function Profile() {
         <span className="text-red-700 cursor-pointer ">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
+      <p className="text-red-700 mt-5">{error && 'Something went wrong!' }</p>
+      <p className="text-green-700 mt-5">{updateSuccess && 'User is updated successfully ' }</p>
+      
     </div>
   )
 }
