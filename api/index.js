cf809e8ebcde09def504040bd8288b37583c6839
jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-02-14 15:28:05 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,12 +6,19 @@ import authRoutes from './routes/auth.route.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
+import path from 'path';
+
 dotenv.config();
 mongoose.connect(process.env.MONGO)
 .then(() => console.log('Connected to MongoDB')).catch((err)=>{
   console.log(err);
 })
+const __dirname =path.resolve();
 const app=express();
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 app.use(express.json());
 
 app.use(cors())
