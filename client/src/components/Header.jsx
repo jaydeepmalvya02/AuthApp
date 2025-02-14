a18f710fa8a61af52e6da196316999e396e2a5fc
jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-02-14 15:17:21 +0530

    home and about page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,10 +4,15 @@ import { Link } from "react-router-dom"
 export default function Header() {
   const {currentUser}=useSelector((state)=>state.user);
   return (
-    <div className='bg-slate-200'>
+    <div className='bg-gradient-to-r from-blue-300 to-purple-400 shadow-lg'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3 '>
-      <Link to='/'>
-      <h1 className='font-bold'>Auth App</h1>
+      <Link to='/' className="text-2xl font-extrabold group cursor-pointer">
+      <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700 drop-shadow-md group-hover:from-blue-400 group-hover:to-blue-600'>
+            Auth
+            </span>
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-purple-700 drop-shadow-md group-hover:from-purple-400 group-hover:to-purple-600'>
+            App
+          </span>
       </Link>
       <ul className='flex gap-4 '>
         <Link to='/'>
