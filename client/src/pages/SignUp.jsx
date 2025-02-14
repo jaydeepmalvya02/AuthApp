3f34ebc0a05a6f6a1a15537d744c1770d1fe8cf9
jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-02-14 15:41:16 +0530

    correction

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 
-import OAuth from '../components/Oauth';
+import OAuth from '../components/Oauth.jsx';
 
 export default function SignUp() {
   const [formData,setFormData]=useState({});
